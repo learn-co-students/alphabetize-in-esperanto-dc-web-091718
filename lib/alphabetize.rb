34c1f463da78e641c74002648d80168be64832fd
Jacqueline Chenault <adigitalnative@gmail.com>
2018-09-03 18:18:01 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |phrase|
+    letter_array = phrase.split("")
+    letter_array.map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
 end
